43d1e01f8347852df1409f19e64bc88251e30dc5
BUNTY <bunty@BUNTYs-MacBook-Air.local>
2026-03-07 21:33:35 +0530

    Inittial commit

---
+++ controllers/storeController.js
@@ -0,0 +1,9 @@
+import productModel from "../models/productModel.js";
+
+const showProducts = async (req, res) => {
+  const products = await productModel.find();
+
+  res.render("store/products", { products: products });
+};
+
+export { showProducts };
