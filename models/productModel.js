c56b409287763954c62f16f811ba5ec9ba01bd52
BUNTY <bunty@BUNTYs-MacBook-Air.local>
2026-03-09 13:37:39 +0530

    initial commit

--- models/productModel.js
+++ models/productModel.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose"
 const productSchema=mongoose.Schema({
   name:{type:String,required:true},
+  desc:{type:String,required:true},
   price:{type:Number, required:true},
+  imageUrl:{type:String}
 });
-
 const productModel = mongoose.model("Product", productSchema);
-
-
-
 // const productModel = [
 //   { id: 1, name: "Product 1", price: 100 },
 //   { id: 2, name: "Product 2", price: 150 },
