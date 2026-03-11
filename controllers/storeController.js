b75b592fd04a5e0686d4ab7e9f19252de01338f0
BUNTY <bunty@Mac.mshome.net>
2026-03-11 13:43:14 +0530

    updated

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -3,6 +3,7 @@ import productModel from "../models/productModel.js";
 const showProducts = async (req, res) => {
   const products = await productModel.find();
 
-  res.render("store/products", { products: products });
+  //res.render("store/products", { products: products });
+  res.json(products);
 };
 export { showProducts };
