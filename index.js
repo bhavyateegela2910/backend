6f5b4a517c74251a4769e6b282f52e7a382f1f41
BUNTY <bunty@BUNTYs-MacBook-Air.local>
2026-03-11 22:56:31 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 import express from "express";
 import expressLayouts from "express-ejs-layouts";
 import session from "express-session";
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import cors from "cors";
+import dotenv from "dotenv";
+import { authenticateAdmin } from "./middleware/auth.js";
 import dbConnect from "./config/db.js";
 import productRouter from "./routes/productRoute.js";
+import storeRouter from "./routes/storeRoute.js";
+import homeRouter from "./routes/homeRoute.js";
+import authRouter from "./routes/authRoute.js";
 import userRouter from "./routes/userRoute.js";
-import { storeRouter } from "./routes/storeRoute.js";
-const app = express();
-import cors from "cors";
-app.use(cors());
-dotenv.config()
 
+const app = express();
+app.use(cors());
+dotenv.config();
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.set("layout","layout")
+app.set("layout", "layout");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 app.use(
@@ -27,14 +30,22 @@ app.use(
   }),
 );
 
-app.use("/", storeRouter);
-// app.use("/auth", authRouter);
-app.use("/products", productRouter);
-app.use("/users", userRouter);
-const startServer=async() =>{
-  await dbConnect()
-app.listen(8080, () => {
-  console.log("Server Started");
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
 });
-}
-startServer()
+
+app.use("/auth", authRouter);
+app.use("/store", storeRouter);
+app.use("/", authenticateAdmin, homeRouter);
+app.use("/products", authenticateAdmin, productRouter);
+app.use("/users", authenticateAdmin, userRouter);
+
+const startServer = async () => {
+  await dbConnect();
+  app.listen(8080, () => {
+    console.log("Server Started");
+  });
+};
+
+startServer();
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,12 +1,19 @@
-import { getProducts,addProduct,addProductForm,deleteProduct,editProductForm,saveProduct} from "../controllers/productController.js";
+import {
+  getProducts,
+  addProductForm,
+  addProduct,
+  deleteProduct,
+  editProductForm,
+  saveProduct,
+} from "../controllers/productController.js";
 import express from "express";
-const productRouter=express.Router();
+const productRouter = express.Router();
 
-productRouter.get("/",getProducts);
+productRouter.get("/", getProducts);
 productRouter.get("/add", addProductForm);
 productRouter.post("/add", addProduct);
 productRouter.get("/:id/delete", deleteProduct);
-productRouter.get("/:id/edit",editProductForm);
-productRouter.post("/:id/edit", editProductForm);
-productRouter.post("/:id/save",saveProduct);
-export  default productRouter
+productRouter.get("/:id/edit", editProductForm);
+productRouter.post("/:id/save", saveProduct);
+
+export default productRouter;
